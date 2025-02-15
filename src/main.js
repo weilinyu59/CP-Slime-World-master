6e67d63c08cafa1c2b8c3900301d98310b9664a3
WeiLin Yu <wyu58@ucsc.edu>
2025-02-15 12:55:04 -0800

    finished

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// Code Practice: Slime World
+// Name: Weilin Yu
+// Date: 2/14/2025
+
+let config = {
+    type: Phaser.AUTO,
+    render: {
+        pixelArt: true
+    },
+    width: 320,
+    height: 240,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    },
+    zoom: 2,
+    scene: [ Overworld ]
+}
+
+const game = new Phaser.Game(config)
